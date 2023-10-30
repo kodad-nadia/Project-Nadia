b1bc0beeca85974b97cf9addf61089b3a759c702
kodad-nadia <nadia.kodad@cap4lab.com>
2023-10-30 14:50:19 +0100

    30

---
+++ configs/firebase_config_example.py
@@ -0,0 +1,9 @@
+firebaseConfig = {
+  "apiKey": "AIzaSyAJ_yX2fa0OUn-SbAUBrkUg-eFkl2y4-FQ",
+  "authDomain": "reservify-34c14.firebaseapp.com",
+  "projectId": "reservify-34c14",
+  "storageBucket": "reservify-34c14.appspot.com",
+  "messagingSenderId": "551077504457",
+  "appId": "1:551077504457:web:b492a5f4ff0d9616cd3418",
+  "databaseurl":"https://console.firebase.google.com/u/1/project/reservify-34c14/overview"
+}
--- main.py
+++ main.py
@@ -5,22 +5,23 @@ from fastapi import FastAPI
 from documentations.description import api_description
 from documentations.tags import tags_metadata
 
-# Routers (You'll create these later)
-import routers.router_reservations, routers.router_user, routers.router_workspace  
+# Routers 
+import routers.router_reservations, routers.router_user, routers.router_workspace, routers.router_auth, routers.router_stripe
 
 # Initialisation de l'API
 app = FastAPI(
     title="Reservify",
     description=api_description,
-    openapi_tags=tags_metadata
+    openapi_tags=tags_metadata,
+    docs_url='/'
 )
 
-# Router dédié aux Reservations (Workspace) - You'll create this router later
+# Router dédié aux Reservations (Workspace) 
 app.include_router(routers.router_reservations.router)
 app.include_router(routers.router_workspace.router)
 app.include_router(routers.router_user.router)
-
-
+app.include_router(routers.router_auth.router)
+app.include_router(routers.router_stripe.router)
 
 
 
