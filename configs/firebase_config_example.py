9da979efb2177199f133b6fb328fedb2374808c2
kodad-nadia <nadia.kodad@cap4lab.com>
2023-11-07 22:41:12 +0100

    07/11 23

--- configs/firebase_config_example.py
+++ configs/firebase_config_example.py
@@ -5,5 +5,7 @@ firebaseConfig = {
   "storageBucket": "reservify-34c14.appspot.com",
   "messagingSenderId": "551077504457",
   "appId": "1:551077504457:web:b492a5f4ff0d9616cd3418",
-  "databaseurl":"https://reservify-34c14-default-rtdb.europe-west1.firebasedatabase.app"
+  "measurementId": "G-RSFYNT66NV",
+  "databaseURL": "https://reservify-34c14-default-rtdb.europe-west1.firebasedatabase.app/"
+
 }
