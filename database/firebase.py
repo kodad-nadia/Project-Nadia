f44e54d6192463db7fb35960da128bb2adcb97b2
kodad-nadia <nadia.kodad@cap4lab.com>
2023-11-07 23:45:27 +0100

    29

--- database/firebase.py
+++ database/firebase.py
@@ -1,15 +1,16 @@
-import firebase_admin
-import pyrebase
-from firebase_admin import credentials
-from dotenv import dotenv_values
 import json
+import firebase_admin
+from firebase_admin import credentials
+import pyrebase 
+
+from dotenv import dotenv_values
 
 config = dotenv_values(".env")
 
-if not firebase_admin._apps:
-  cred = credentials.Certificate(json.loads(config['FIREBASE_SERVICE_ACCOUNT_KEY']))
-  firebase_admin.initialize_app(cred)
+# Initialize Firebase Admin with the service account information
+cred = credentials.Certificate(json.loads(config['FIREBASE_SERVICE_ACCOUNT_KEY']))
+firebase_admin.initialize_app(cred)
 
 firebase = pyrebase.initialize_app(json.loads(config['FIREBASE_CONFIG']))
 db = firebase.database()
-authUser = firebase.auth()
+authSession = firebase.auth()
