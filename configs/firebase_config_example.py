a8ee2e7a5f37d22a7e1b705bdc4cedc241413e66
kodad-nadia <nadia.kodad@cap4lab.com>
2023-11-07 22:41:49 +0100

    07/11 24

--- configs/firebase_config_example.py
+++ configs/firebase_config_example.py
@@ -6,6 +6,6 @@ firebaseConfig = {
   "messagingSenderId": "551077504457",
   "appId": "1:551077504457:web:b492a5f4ff0d9616cd3418",
   "measurementId": "G-RSFYNT66NV",
-  "databaseURL": "https://reservify-34c14-default-rtdb.europe-west1.firebasedatabase.app/"
+  "databaseURL": "https://reservify-34c14-default-rtdb.europe-west1.firebasedatabase.app"
 
 }
